682a5ba92c4728ef84fcf9484197f10a9a9a2d23
Mike Winter <mike.winter1@nhs.net>
2021-10-05 10:58:06 +0100

    Initial commit

---
+++ src/main/java/uk/nhs/digital/hapispike/HapiSpikeApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.hapispike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapiSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapiSpikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/nhs/digital/hapispike/HapiSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhs.digital.hapispike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapiSpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
